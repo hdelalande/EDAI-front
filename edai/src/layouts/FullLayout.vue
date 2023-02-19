d3347023ed03892e07698bd6ad394cd3b3d7181f
Bryan Djafer <62521415+b-yukky@users.noreply.github.com>
2023-02-19 22:34:57 +0900

    main layout

---
+++ edai/src/layouts/FullLayout.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { RouterView } from "vue-router";
+import HorizontalHeader from "./horizontal-header/HorizontalHeader.vue";
+
+</script>
+
+<template>
+  <v-app>
+    <v-layout>
+        <HorizontalHeader />
+
+        <v-main>
+          <v-container fluid class="page-wrapper">
+            <RouterView />
+          </v-container>
+        </v-main>
+    </v-layout>
+  </v-app>
+</template>
