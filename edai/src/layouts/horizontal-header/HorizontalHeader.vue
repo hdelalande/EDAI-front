7ecc7ec9cb8a74005b16e49c998a6b519bcb73c9
Bryan Djafer <62521415+b-yukky@users.noreply.github.com>
2023-02-20 21:49:15 +0900

    header loading connection

--- edai/src/layouts/horizontal-header/HorizontalHeader.vue
+++ edai/src/layouts/horizontal-header/HorizontalHeader.vue
@@ -1,4 +1,9 @@
 <script setup>
+import { useConnectionStateStore } from '@/stores/connection'
+import { storeToRefs } from 'pinia'
+
+const store = useConnectionStateStore()
+const { connected, loading } = storeToRefs(store)
 
 </script>
 
@@ -23,10 +28,19 @@
 
     <v-spacer />
 
-    <v-chip class="mx-2">
-      <v-badge color="error" dot inline>
-        <span class="mr-2"> not connected </span>
-      </v-badge>
+    <v-chip :color="connected ? 'success' : 'error'" class="mr-2"> 
+
+      {{ connected ?  'connected': 'not connected' }} 
+      <v-progress-circular
+        v-if="loading"
+        indeterminate
+        :size="10"
+        :width="2"
+        color="primary"
+        class="ml-2"
+      ></v-progress-circular>
+      <v-badge v-if="!loading" dot inline :color=" connected ? 'success': 'error'"></v-badge>
+      
     </v-chip>
 
   </v-app-bar>
