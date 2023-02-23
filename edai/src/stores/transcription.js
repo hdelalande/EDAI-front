c29cf15984470e7eb489ae5f638c9a2f6d40269c
Bryan Djafer <62521415+b-yukky@users.noreply.github.com>
2023-02-23 22:50:20 +0900

    update store transcription

--- edai/src/stores/transcription.js
+++ edai/src/stores/transcription.js
@@ -6,13 +6,24 @@ export const useTranscriptionStore = defineStore('transcription', () => {
   const speechSegmentsArray = ref(['This is the first segment', 'This is the second segment', 'This is the third segment', 'This is the 4th segment', 'This is the 5th segment', 'This is the 6th segment', 'This is the 7th segment', 'This is the 8th segment', 'This is the 9th segment', 'This is the 10th segment', 'This is the 11th segment', 'This is the 12th segment'])
   const lastSpeechSegment = ref('This is the last segment')
 
+  const selectedItems = ref([])
+
   const transcription = computed(() => {
     return speechSegmentsArray.value.join('. ') + '. ' + lastSpeechSegment.value
   })
+
+  const selectedText = computed(() => {
+    return selectedItems.value
+    .map(index => speechSegmentsArray.value[index])
+    .join('')
+  })
+
   return {
     speechSegmentsArray,
     lastSpeechSegment,
-    transcription
+    transcription,
+    selectedItems,
+    selectedText
   }
 
 })
